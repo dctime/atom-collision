e6a90e6df4969df26f7082a50351c9fce3c6c3ef
dctime <daniel.chu916@gmail.com>
2023-06-13 14:43:22 +0800

    Feature: Ha now we got exe files

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 from game import Game
 
-WIDTH = 1200
-HEIGHT = 800
+if __name__ == "__main__":
+    WIDTH = 1200
+    HEIGHT = 800
 
-# Initialize Pygame
-pygame.init()
+    # Initialize Pygame
+    pygame.init()
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-# screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen_x_size, screen_y_size = screen.get_size()
-MID_SCREEN_POINT = (screen_x_size/2, screen_y_size/2)
-UNIT_SIZE = 30
-FRAMERATE = 500
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    # screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen_x_size, screen_y_size = screen.get_size()
+    MID_SCREEN_POINT = (screen_x_size/2, screen_y_size/2)
+    UNIT_SIZE = 30
+    FRAMERATE = 500
 
-# Set window title
-pygame.display.set_caption("My Pygame Screen")
+    # Set window title
+    pygame.display.set_caption("My Pygame Screen")
 
-# set up the environment
-game = Game(screen, 1/FRAMERATE, MID_SCREEN_POINT, UNIT_SIZE)
-game.run()
+    # set up the environment
+    game = Game(screen, 1/FRAMERATE, MID_SCREEN_POINT, UNIT_SIZE)
+    game.run()
+
+
+
 
